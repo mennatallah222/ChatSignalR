ce92563e7d3e285643755fa98a5d1bef4c20dd10
mennatallah222 <mennatallahahmed892@gmail.com>
2024-08-09 19:40:53 +0300

    added group and almost done with it

--- SignalRTrial/ChatHub.cs
+++ SignalRTrial/ChatHub.cs
@@ -1,12 +1,41 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System.Collections.Concurrent;
 
 namespace SignalRTrial
 {
+    public record User(string name, string room);
+    public record Message(string name, string text);
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        private static ConcurrentDictionary<string, User> _users = new();
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            if (_users.TryRemove(Context.ConnectionId, out var user))
+            {
+                await Groups.RemoveFromGroupAsync(Context.ConnectionId, user.room);
+                await Clients.Group(user.room).SendAsync("UserLeft", user.name);
+            }
+
+            await base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task JoinedRoom(string roomName, string userName)
+        {
+            var user = new User(userName, roomName);
+            _users[Context.ConnectionId] = user;
+
+            await Groups.AddToGroupAsync(Context.ConnectionId, roomName);
+            await Clients.Group(roomName).SendAsync("UserJoined", userName);
+        }
+
+        public async Task SendMessageToRoom(string roomName, string message)
+        {
+            var user = _users[Context.ConnectionId];
+            var msg = new Message(user.name, message);
+
+            await Clients.Group(roomName).SendAsync("ReceiveMessage", msg);
         }
     }
+
 }
